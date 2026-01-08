c4989c1588427e6fd299c0d883dcd5d14fa5686a
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-01-08 17:16:52 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Typography from '@mui/material/Typography';
+
+
+function App() {
+  return (
+    <>
+      <Typography variant="h1" component="h2">
+        السلام عليكم
+      </Typography>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,20 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: "IBM Plex Sans Arabic",
+  }
+})
+
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
+  </StrictMode>,
+)
