77ee0e059b1f74f78be1393c52202361ed41da64
MedoHaytham <104214885+MedoHaytham@users.noreply.github.com>
2026-01-09 18:50:43 +0200

    make it dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,10 @@ import CloudIcon from '@mui/icons-material/Cloud';
 import Button from '@mui/material/Button';
 import Container from '@mui/material/Container';
 import { useEffect, useState } from 'react';
+import { useTranslation } from 'react-i18next';
 import axios from 'axios';
 import dayjs from 'dayjs';
-import { useTranslation } from 'react-i18next';
+import {toast}  from 'react-toastify';
 
 import 'dayjs/locale/ar';
 
@@ -19,7 +20,7 @@ const toArabicNumbers = (string) => {
 function App() {
 
   const { t, i18n } = useTranslation();
-
+  const [coords, setCoords] = useState({ lat: null, lon: null });
   const [weather, setWeather] = useState({
     name: '',
     temp: null,
@@ -32,10 +33,26 @@ function App() {
   const [locale, setLocale] = useState('ar');
   const direction = locale === 'en' ? 'ltr' : 'rtl';
 
+  useEffect(() => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setCoords({
+            lat: position.coords.latitude,
+            lon: position.coords.longitude,
+          });
+        },
+        (err) => toast.error('Geolocation error:' + err),
+        { enableHighAccuracy: true }
+      );
+    }
+  }, []);
+
   useEffect(()=>{
+    if (!coords.lat || !coords.lon) return;
     async function fetchWeather() {
       try {
-        let response = await axios.get('https://api.openweathermap.org/data/2.5/weather?lat=29.791769&lon=31.299813&appid=e6e5cdf4bbf0ab6cf484e5b6adea6562')
+        let response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=e6e5cdf4bbf0ab6cf484e5b6adea6562&lang=${i18n.language}`)
         const name = response.data.name;
         const temp = Math.round(response.data.main.temp - 272.15);
         const description = response.data.weather[0].description;
@@ -51,11 +68,11 @@ function App() {
           icon,
         });
       } catch (error) {
-        console.log(error)
+        toast.error('Error on fetch Weather' + error);
       }
     }
     fetchWeather();
-  },[]);
+  },[coords.lat, coords.lon, i18n.language]);
 
   useEffect(() => {
     dayjs.locale('ar');
@@ -67,26 +84,20 @@ function App() {
   },[]);
 
   function langHandler() {
-    setLocale('');
-    if (locale === 'en') {
-      setLocale('ar');
-      i18n.changeLanguage('ar');
-      dayjs.locale('ar');
-    } else {
-      setLocale('en');
-      i18n.changeLanguage('en');
-      dayjs.locale('en');
-    }
+    const newLocale = locale === 'en' ? 'ar' : 'en';
+    setLocale(newLocale);
+    i18n.changeLanguage(newLocale);
+    dayjs.locale(newLocale);
     setDateAndTime(dayjs().format('dddd D MMMM YYYY'));
   }
 
   return (
-    <section className=''>
+    <section>
       <Container maxWidth="sm" className='h-screen relative flex flex-col justify-center items-center'>
         <div className="card w-full rounded-[15px] text-white bg-[rgba(28,52,91,0.36)] p-2.5 shadow-[0px_11px_1px_rgba(0,0,0,0.05)]">
           <div className="content">
             <div className="city-time flex justify-start items-end gap-5 mb-2.5" dir={direction}>
-              <Typography variant="h2" className=' text-[35px]! md:text-[60px]! mr-5! font-semibold!'>
+              <Typography variant="h2" className=' text-[20px]! md:text-[45px]! mr-5! font-semibold!'>
                 {t(weather.name)}
               </Typography>
               <Typography variant="h5" className='text-[16px]! md:text-[24px]!'> 
@@ -106,12 +117,12 @@ function App() {
                   {t(weather.description)}
                 </Typography>
                 <div className='min-max flex items-center'>
-                  <h5 className='capitalize'>{t('min')} : {locale === 'en' ? weather.temp : weather.min !== null ? toArabicNumbers(weather.min.toString()) : ''}</h5> 
+                  <h5 className='capitalize'>{t('min')} : {locale === 'en' ? weather.min : weather.min !== null ? toArabicNumbers(weather.min.toString()) : ''}</h5> 
                   <h5 className='mx-1.5'>|</h5> 
-                  <h5 className='capitalize'>{t('max')} : {locale === 'en' ? weather.temp : weather.max !== null ? toArabicNumbers(weather.max.toString()) : ''}</h5>
+                  <h5 className='capitalize'>{t('max')} : {locale === 'en' ? weather.max : weather.max !== null ? toArabicNumbers(weather.max.toString()) : ''}</h5>
                 </div>
               </div>
-              <CloudIcon className='text-[150px]! md:text-[200px]! text-white!'/>
+              <CloudIcon className='text-[135px]! md:text-[200px]! text-white!'/>
             </div>
           </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { ToastContainer } from 'react-toastify';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 import './i18n';
 
+import App from './App.jsx'
 
 const theme = createTheme({
   typography: {
@@ -13,10 +14,10 @@ const theme = createTheme({
   }
 })
 
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
+      <ToastContainer />
       <App />
     </ThemeProvider>
   </StrictMode>,
